95d2d8ead4f2933ec37a4f6dab4b28534df99772
moon <rachitmishra5594@gmail.com>
2018-07-19 22:34:08 +0530

    Added Python script to perform Arithmetic Operations

---
+++ arithmetic-operations.py
@@ -0,0 +1,20 @@
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+add = n1 + n2
+print('Addition of ', n1, ' & ', n2, ' is ', add)
+
+sub = n1 - n2
+print('Subtraction of ', n1, ' & ', n2, ' is ', sub)
+
+mul = n1 * n2
+print('Multiplication of ', n1, ' & ', n2, ' is ', mul)
+
+divFloat = n1 / n2
+print('Float Division of ', n1, ' & ', n2, ' is ', divFloat)
+
+divInt = n1 // n2
+print('Integer Division of ', n1, ' & ', n2, ' is ', divInt)
+
+power = n1 ** n2
+print(n1, ' to the power ', n2, ' is ', power)
